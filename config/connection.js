88b72c49767621a25ccf3ff62e2ef1a72b8e9e9d
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-10-05 23:04:48 -0500

    udpated server.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,24 @@
-const mysql = require('mysql')
-const connection;
+var mysql = require('mysql')
+var connection
 
-if(process.env.JAWSDB_ULR) {
-    connection = mysql.createConnection(process.env.JAWSDB_ULR)
+if (process.env.JAWSDB_ULR) {
+	connection = mysql.createConnection(process.env.JAWSDB_ULR)
 } else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        port: 3306,
-        user: 'root',
-        password: 'password',
-        database: 'burger_db'
-    })
+	connection = mysql.createConnection({
+		host: 'localhost',
+		port: 3306,
+		user: 'root',
+		password: 'password',
+		database: 'burger_db',
+	})
 }
 
-connection.connect()
+connection.connect(function (err) {
+	if (err) {
+		console.error(err.stack)
+		return
+	}
+	console.log(connection.threadId)
+})
 
-module.exports = connection;
+module.exports = connection
