47bb34932b55f7e57c3cc74661f8a012e8f048e5
Pierre Pariente <pierre.parientedimitrov@gmail.com>
2020-10-10 11:39:05 -0500

    corrected typo in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,11 @@
 var mysql = require('mysql')
 var connection
 
-if (process.env.JAWSDB_ULR) {
-	connection = mysql.createConnection(process.env.JAWSDB_ULR)
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
 	connection = mysql.createConnection({
 		host: 'localhost',
-		port: 3306,
 		user: 'root',
 		password: 'password',
 		database: 'burger_db',
